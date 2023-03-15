c4b28b5595a22a4abf0bd33ec915d5e3c5009b7f
Aamir <aamir104512@gmail.com>
2023-03-15 15:58:39 +0500

    towing rates desgin and api

--- src/screens/Authorized/CustomerScreens/CustomerDrawer/TowingRates.js
+++ src/screens/Authorized/CustomerScreens/CustomerDrawer/TowingRates.js
@@ -1,148 +1,142 @@
-import {View, Text, TouchableOpacity} from 'react-native';
-import React from 'react';
+import {View, Text, TouchableOpacity, ActivityIndicator} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import {SIZES, COLORS} from '../../../../constants/theme';
 import Entypo from 'react-native-vector-icons/Entypo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function TowingRates() {
+  const [towingRates, setTowingRates] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+        if (token !== null) {
+          console.log('Token retrieved from AsyncStorage:', token);
+          try {
+            const response = await fetch(
+              'https://app.ecsapshipping.com/api/auth/rate/towing',
+              {
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                  Authorization: 'Bearer ' + token,
+                },
+              },
+            );
+
+            console.log('Fetching Towing Prices...');
+            const data = await response.json();
+
+            if (data.status == 'Success') {
+              setTowingRates(data);
+              console.log('Towing Prices successfully');
+              console.log(data.message);
+              // console.log(data.data.master_towing);
+              // setRefreshing(false);
+            } else {
+              console.log('Error fetching towing prices');
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      } catch (error) {
+        console.warn('Error while retrieving token from AsyncStorage:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View style={{flex: 1, backgroundColor: '#C3E7F8'}}>
       <View style={{alignItems: 'center', marginTop: 25}}>
         {/* first item in list */}
-        <TouchableOpacity
-          style={{
-            height: SIZES.windowHeight / 6,
-            width: SIZES.windowWidth / 1.1,
-            backgroundColor: COLORS.primary,
-            borderRadius: 20,
-            justifyContent: 'center',
-          }}>
-          <View
-            style={{
-              height: 25,
-              width: 25,
-              backgroundColor: COLORS.white,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: 15,
-              position: 'absolute',
-              top: 10,
-              right: 10,
-            }}>
-            <Entypo name="check" size={20} color="green" />
-          </View>
-          <View
-            style={{
-              paddingHorizontal: 20,
-              justifyContent: 'center',
-            }}>
-            {/* Item 1 */}
-            <View
-              style={{
-                flexDirection: 'row',
-                // justifyContent: 'space-between',
-                alignItems: 'center',
-              }}>
-              <View style={{width: '50%'}}>
-                <Text style={{color: COLORS.white}}>City: New Jersey</Text>
-              </View>
-              <View>
-                <Text style={{color: COLORS.white}}>State: NY</Text>
-              </View>
-            </View>
 
-            {/* Item 2 */}
-            <View
-              style={{
-                flexDirection: 'row',
-                // justifyContent: 'space-between',
-                alignItems: 'center',
-              }}>
-              <View style={{width: '50%'}}>
-                <Text style={{color: COLORS.white}}>Auction: Copart</Text>
-              </View>
+        {towingRates != null &&
+          towingRates.data.master_towing.map(item => {
+            return (
               <View
+                key={item.id}
                 style={{
-                  height: '60%',
-                  width: 80,
-                  backgroundColor: 'white',
-                  alignItems: 'center',
+                  height: SIZES.windowHeight / 10,
+                  width: SIZES.windowWidth / 1.1,
+                  backgroundColor: COLORS.primary,
+                  borderRadius: 20,
                   justifyContent: 'center',
-                  borderRadius: 10,
+                  marginBottom: 10,
                 }}>
-                <Text style={{color: COLORS.black}}>500$</Text>
-              </View>
-            </View>
-          </View>
-        </TouchableOpacity>
+                <View
+                  style={{
+                    height: 25,
+                    width: 25,
+                    backgroundColor: COLORS.white,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderRadius: 15,
+                    position: 'absolute',
+                    top: 10,
+                    right: 10,
+                  }}>
+                  {item.status == 1 ? (
+                    <Entypo name="check" size={20} color="green" />
+                  ) : (
+                    <Entypo name="cross" size={20} color="red" />
+                  )}
+                </View>
+                <View
+                  style={{
+                    paddingHorizontal: 20,
+                    justifyContent: 'center',
+                  }}>
+                  {/* Item 1 */}
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      // justifyContent: 'space-between',
+                      alignItems: 'center',
+                    }}>
+                    <View style={{width: '50%'}}>
+                      <Text style={{color: COLORS.white}}>
+                        City: {item.city}
+                      </Text>
+                    </View>
+                    <View>
+                      <Text style={{color: COLORS.white}}>
+                        State: {item.new_jersery}
+                      </Text>
+                    </View>
+                  </View>
 
-        {/* Second item in list */}
-        <TouchableOpacity
-          style={{
-            height: SIZES.windowHeight / 6,
-            width: SIZES.windowWidth / 1.1,
-            backgroundColor: COLORS.primary,
-            borderRadius: 20,
-            justifyContent: 'center',
-            marginTop: 10,
-          }}>
-          <View
-            style={{
-              height: 25,
-              width: 25,
-              backgroundColor: COLORS.white,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: 15,
-              position: 'absolute',
-              top: 10,
-              right: 10,
-            }}>
-            <Entypo name="cross" size={20} color="red" />
-          </View>
-          <View
-            style={{
-              paddingHorizontal: 20,
-              justifyContent: 'center',
-            }}>
-            {/* Item 1 */}
-            <View
-              style={{
-                flexDirection: 'row',
-                // justifyContent: 'space-between',
-                alignItems: 'center',
-              }}>
-              <View style={{width: '50%'}}>
-                <Text style={{color: COLORS.white}}>City: New Jersey</Text>
+                  {/* Item 2 */}
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      marginTop: 5,
+                      alignItems: 'center',
+                    }}>
+                    <View style={{width: '50%'}}>
+                      <Text style={{color: COLORS.white}}>
+                        Auction: {item.auction}
+                      </Text>
+                    </View>
+                    <View
+                      style={{
+                        height: '60%',
+                        width: 80,
+                        backgroundColor: 'white',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderRadius: 10,
+                      }}>
+                      <Text style={{color: COLORS.black}}>{item.rate}$</Text>
+                    </View>
+                  </View>
+                </View>
               </View>
-              <View>
-                <Text style={{color: COLORS.white}}>State: NY</Text>
-              </View>
-            </View>
-
-            {/* Item 2 */}
-            <View
-              style={{
-                flexDirection: 'row',
-                // justifyContent: 'space-between',
-                alignItems: 'center',
-              }}>
-              <View style={{width: '50%'}}>
-                <Text style={{color: COLORS.white}}>Auction: Copart</Text>
-              </View>
-              <View
-                style={{
-                  height: '60%',
-                  width: 80,
-                  backgroundColor: 'white',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  borderRadius: 10,
-                }}>
-                <Text style={{color: COLORS.black}}>500$</Text>
-              </View>
-            </View>
-          </View>
-        </TouchableOpacity>
+            );
+          })}
       </View>
     </View>
   );
