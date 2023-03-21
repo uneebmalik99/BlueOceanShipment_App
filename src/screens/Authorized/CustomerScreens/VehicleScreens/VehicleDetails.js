02d17acb88ff171ec1a4f12c146f794169aa3cab
Aamir <aamir104512@gmail.com>
2023-03-21 18:03:55 +0500

    customer vehicle details api

--- src/screens/Authorized/CustomerScreens/VehicleScreens/VehicleDetails.js
+++ src/screens/Authorized/CustomerScreens/VehicleScreens/VehicleDetails.js
@@ -1,13 +1,60 @@
-import {View, Text, Image, TouchableOpacity} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import {SIZES, COLORS} from '../../../../constants/theme';
 import LinearGradient from 'react-native-linear-gradient';
 import VehicleHeader from '../../../../components/VehicleHeader';
 import IonIcons from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useIsFocused} from '@react-navigation/native';
 
-export default function VehicleDetails({navigation, route}) {
+export default function AdminVehicleDetails({navigation, route}) {
   //data coming from vehicle screens
-  const {Data} = route.params;
+  const {ID} = route.params;
+  const [details, setDetails] = useState(null);
+  const isFocused = useIsFocused();
+
+  var asset_url = 'https://app.ecsapshipping.com/public/';
+
+  useEffect(() => {
+    const ViewDetails = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+          console.log('Token retrieved from AsyncStorage:', token);
+
+          const url = `https://app.ecsapshipping.com/api/auth/vehicle/show/${ID}`;
+
+          const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + token,
+            },
+          });
+
+          const data = await response.json();
+
+          if (data.status == 'Success') {
+            console.log('Fetched Vehicle Details Successfully');
+            setDetails(data);
+            // console.log(JSON.stringify(data));
+          } else {
+            console.log('UnSuccess ', data);
+          }
+        }
+      } catch (error) {
+        console.warn('Error while retrieving token from AsyncStorage:', error);
+      }
+    };
+
+    ViewDetails();
+  }, [isFocused]);
 
   function ContactItems({ItemText}) {
     return (
@@ -22,8 +69,6 @@ export default function VehicleDetails({navigation, route}) {
           shadowColor: COLORS.primary,
           elevation: 5,
           justifyContent: 'center',
-          //   borderBottomWidth: 1,
-          //   borderBottomColor: COLORS.primary,
         }}>
         <Text style={{color: COLORS.black, textAlign: 'justify', fontSize: 14}}>
           {ItemText}
@@ -35,91 +80,130 @@ export default function VehicleDetails({navigation, route}) {
   return (
     <View style={{flex: 1, backgroundColor: COLORS.white}}>
       {/* cover of the image */}
-      <View>
-        <Image
-          source={Data.cover}
-          style={{width: SIZES.windowWidth, height: SIZES.windowHeight / 4}}
-        />
-      </View>
+      {details != null && (
+        <View>
+          {!details.data.warehouse_image ||
+          details.data.warehouse_image.length === 0 ? (
+            <View
+              style={{
+                width: SIZES.windowWidth,
+                height: SIZES.windowHeight / 4,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Text style={{color: COLORS.black}}>No Image</Text>
+            </View>
+          ) : (
+            <Image
+              source={{
+                uri: asset_url + details.data.warehouse_image[0].name,
+              }}
+              resizeMode="cover"
+              style={{width: SIZES.windowWidth, height: SIZES.windowHeight / 4}}
+            />
+          )}
+        </View>
+      )}
 
       <VehicleHeader
         HeaderTitle={'Vehicle Details'}
         GoBack={() => navigation.goBack()}
       />
 
-      <View
-        style={{
-          height: SIZES.windowHeight / 12,
-          width: SIZES.windowWidth,
-          backgroundColor: COLORS.primary,
-          //   borderTopLeftRadius: 30,
-          //   borderTopRightRadius: 30,
-          //   bottom: SIZES.windowHeight / 6.2 - 100,
-          justifyContent: 'center',
-        }}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            paddingHorizontal: 20,
-          }}>
-          <View>
-            <Text style={{color: COLORS.white, fontSize: 16}}>{Data.vin}</Text>
-          </View>
-          <TouchableOpacity>
-            <IonIcons
-              name="md-qr-code-outline"
-              size={25}
-              color={COLORS.white}
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      <View style={{alignItems: 'center', marginTop: 20}}>
-        <ContactItems
-          ItemText={
-            'Vehicle Details: ' + Data.year + ' ' + Data.make + ' ' + Data.model
-          }
-        />
-
-        <View style={{marginTop: 15}}>
-          <ContactItems ItemText={'Shipper Name: ' + Data.shipper_name} />
-        </View>
-
-        <View style={{marginTop: 15}}>
-          <ContactItems ItemText={'Lot Number: ' + Data.lot} />
-        </View>
-      </View>
-
-      <View
-        style={{paddingHorizontal: 20, paddingTop: 20, alignItems: 'center'}}>
-        <TouchableOpacity
-          style={{
-            height: SIZES.windowHeight / 18,
-            width: SIZES.windowWidth / 1.3,
-            borderRadius: 10,
-          }}
-          onPress={() =>
-            navigation.navigate('EditVehicleDetails', {VehicleData: Data})
-          }>
-          <LinearGradient
-            start={{x: 0, y: 0}}
-            end={{x: 1, y: 0}}
-            colors={['#1A72DE', 'rgba(35, 111, 204, 0.19)']}
+      {details != null ? (
+        <View>
+          <View
             style={{
-              flex: 1,
-              alignItems: 'center',
+              height: SIZES.windowHeight / 12,
+              width: SIZES.windowWidth,
+              backgroundColor: COLORS.primary,
               justifyContent: 'center',
-              borderRadius: 10,
             }}>
-            <Text style={{color: COLORS.white, fontSize: 16}}>
-              Edit Details
-            </Text>
-          </LinearGradient>
-        </TouchableOpacity>
-      </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                paddingHorizontal: 20,
+              }}>
+              <View>
+                <Text style={{color: COLORS.white, fontSize: 16}}>
+                  {details.data.vin}
+                </Text>
+              </View>
+              <TouchableOpacity>
+                <IonIcons
+                  name="md-qr-code-outline"
+                  size={25}
+                  color={COLORS.white}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <View style={{alignItems: 'center', marginTop: 20}}>
+            <ContactItems
+              ItemText={
+                'Vehicle Details: ' +
+                details.data.year +
+                ' ' +
+                details.data.make +
+                ' ' +
+                details.data.model
+              }
+            />
+
+            <View style={{marginTop: 15}}>
+              <ContactItems
+                ItemText={'Shipper Name: ' + details.data.shipper_name}
+              />
+            </View>
+
+            <View style={{marginTop: 15}}>
+              <ContactItems ItemText={'Lot Number: ' + details.data.lot} />
+            </View>
+          </View>
+
+          <View
+            style={{
+              paddingHorizontal: 20,
+              paddingTop: 20,
+              alignItems: 'center',
+            }}>
+            <TouchableOpacity
+              style={{
+                height: SIZES.windowHeight / 18,
+                width: SIZES.windowWidth / 1.3,
+                borderRadius: 10,
+              }}
+              onPress={() =>
+                navigation.navigate('EditVehicleDetails', {
+                  ID: ID,
+                  Details: details,
+                })
+              }>
+              <LinearGradient
+                start={{x: 0, y: 0}}
+                end={{x: 1, y: 0}}
+                colors={['#1A72DE', 'rgba(35, 111, 204, 0.19)']}
+                style={{
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: 10,
+                }}>
+                <Text style={{color: COLORS.white, fontSize: 16}}>
+                  Edit Details
+                </Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <ActivityIndicator size={'large'} color={COLORS.primary} />
+        </View>
+      )}
     </View>
   );
 }
