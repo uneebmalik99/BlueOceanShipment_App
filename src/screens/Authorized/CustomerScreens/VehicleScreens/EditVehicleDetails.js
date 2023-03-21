596a96569a9f30d08843969e8ce5ab4ec022be47
Aamir <aamir104512@gmail.com>
2023-03-21 18:03:43 +0500

    update customer vehicle api

--- src/screens/Authorized/CustomerScreens/VehicleScreens/EditVehicleDetails.js
+++ src/screens/Authorized/CustomerScreens/VehicleScreens/EditVehicleDetails.js
@@ -1,42 +1,123 @@
-import {View, Text, Image, TouchableOpacity, TextInput} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+import React, {useState} from 'react';
 import {SIZES, COLORS} from '../../../../constants/theme';
 import LinearGradient from 'react-native-linear-gradient';
 import VehicleHeader from '../../../../components/VehicleHeader';
 import IonIcons from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function EditVehicleDetails({navigation, route}) {
-  const {VehicleData} = route.params;
+  const {ID, Details} = route.params;
+  var asset_url = 'https://app.ecsapshipping.com/public/';
+  //   console.log(VehicleData.title);
+  const [lot, setLot] = useState('');
+  //   console.log(lot);
+  const [title, setTitle] = useState('');
+  const [key, setKey] = useState('');
+  const [year, setYear] = useState('');
+  const [make, setMake] = useState('');
+  const [model, setModel] = useState('');
 
-  function ContactItems({Placeholder}) {
-    return (
-      <TextInput
-        placeholder={Placeholder}
-        style={{
-          height: SIZES.windowHeight / 12,
-          width: SIZES.windowWidth / 1.1,
-          backgroundColor: COLORS.lightGray,
-          paddingLeft: 10,
-          borderRadius: 10,
-          shadowColor: COLORS.primary,
-          elevation: 5,
-          borderWidth: 1,
-          borderColor: COLORS.lightGray,
-          justifyContent: 'center',
-          color: 'black',
-        }}
-      />
-    );
-  }
+  const update = async () => {
+    var value = new FormData();
+
+    value.append('vehicle_id', ID);
+    {
+      lot.length != 0 && value.append('lot', lot);
+    }
+    {
+      title.length != 0 && value.append('title', title);
+    }
+    {
+      key.length != 0 && value.append('key', key);
+    }
+    {
+      year.length != 0 && value.append('year', year);
+    }
+    {
+      make.length != 0 && value.append('make', make);
+    }
+    {
+      model.length != 0 && value.append('model', model);
+    }
+
+    // value.append('customer_name', companyName);
+    // value.append('buyer_id', buyer);
+
+    // value.append('warehouse_images[]', warehouseImage);
+    // value.append('pickup[]', pickupImage);
+    // value.append('auction_images[]', auctionImage);
+
+    console.log('===' + JSON.stringify(value));
+    try {
+      const token = await AsyncStorage.getItem('token');
+      if (token !== null) {
+        console.log('Token retrieved from AsyncStorage:', token);
+
+        var url = 'https://app.ecsapshipping.com/api/auth/vehicle/update';
+
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + token,
+          },
+          body: value,
+        })
+          .then(response => response.json())
+          .then(responseJson => {
+            if (responseJson.status == 'Success') {
+              //   setWareHouseImage([]);
+              //   setAuctionImage([]);
+              //   setPickupImage([]);
+              console.log('Update Success');
+              alert('Update Success');
+            } else {
+              console.log('UnSuccess ', responseJson);
+            }
+          })
+          .catch(error => {
+            alert(error);
+            console.warn(error);
+          });
+      }
+    } catch (error) {
+      console.warn('Error while retrieving token from AsyncStorage:', error);
+    }
+  };
 
   return (
     <View style={{flex: 1, backgroundColor: COLORS.white}}>
       {/* cover of the image */}
       <View>
-        <Image
-          source={VehicleData.cover}
-          style={{width: SIZES.windowWidth, height: SIZES.windowHeight / 4}}
-        />
+        {!Details.data.warehouse_image ||
+        Details.data.warehouse_image.length === 0 ? (
+          <View
+            style={{
+              width: SIZES.windowWidth,
+              height: SIZES.windowHeight / 4,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text style={{color: COLORS.black}}>No Image</Text>
+          </View>
+        ) : (
+          <Image
+            source={{
+              uri: asset_url + Details.data.warehouse_image[0].name,
+            }}
+            resizeMode="cover"
+            style={{width: SIZES.windowWidth, height: SIZES.windowHeight / 4}}
+          />
+        )}
       </View>
 
       <VehicleHeader
@@ -49,9 +130,6 @@ export default function EditVehicleDetails({navigation, route}) {
           height: SIZES.windowHeight / 12,
           width: SIZES.windowWidth,
           backgroundColor: COLORS.primary,
-          //   borderTopLeftRadius: 30,
-          //   borderTopRightRadius: 30,
-          //   bottom: SIZES.windowHeight / 6.2 - 100,
           justifyContent: 'center',
         }}>
         <View
@@ -63,7 +141,7 @@ export default function EditVehicleDetails({navigation, route}) {
           }}>
           <View>
             <Text style={{color: COLORS.white, fontSize: 16}}>
-              {VehicleData.vin}
+              {Details.data.vin}
             </Text>
           </View>
           <TouchableOpacity>
@@ -76,45 +154,137 @@ export default function EditVehicleDetails({navigation, route}) {
         </View>
       </View>
 
-      <View style={{alignItems: 'center', marginTop: 20}}>
-        <ContactItems Placeholder={'Muhammad Aamir'} />
-
-        <View style={{marginTop: 15}}>
-          <ContactItems Placeholder={'Texas'} />
-        </View>
-
-        <View style={{marginTop: 15}}>
-          <ContactItems Placeholder={'12345678'} />
-        </View>
-      </View>
-
-      <View
-        style={{paddingHorizontal: 20, paddingTop: 20, alignItems: 'center'}}>
-        <TouchableOpacity
-          style={{
-            height: SIZES.windowHeight / 18,
-            width: SIZES.windowWidth / 1.3,
-            borderRadius: 10,
-          }}
-          onPress={() =>
-            navigation.navigate('EditVehicleDetails', {
-              VehicleData: VehicleData,
-            })
-          }>
-          <LinearGradient
-            start={{x: 0, y: 0}}
-            end={{x: 1, y: 0}}
-            colors={['#1A72DE', 'rgba(35, 111, 204, 0.19)']}
+      <ScrollView>
+        <View style={{alignItems: 'center', marginTop: 20}}>
+          {/* lot textinput */}
+          <TextInput
+            placeholder={'Enter lot'}
+            onChangeText={text => setLot(text)}
+            keyboardType={'number-pad'}
+            value={lot}
             style={{
-              flex: 1,
-              alignItems: 'center',
-              justifyContent: 'center',
+              height: SIZES.windowHeight / 12,
+              width: SIZES.windowWidth / 1.1,
+              backgroundColor: COLORS.lightGray,
+              paddingLeft: 10,
               borderRadius: 10,
-            }}>
-            <Text style={{color: COLORS.white, fontSize: 16}}>Save</Text>
-          </LinearGradient>
-        </TouchableOpacity>
-      </View>
+              shadowColor: COLORS.primary,
+              elevation: 5,
+              borderWidth: 1,
+              borderColor: COLORS.lightGray,
+              justifyContent: 'center',
+              color: 'black',
+            }}
+          />
+
+          {/* title textinput */}
+          <View style={{marginTop: 15}}>
+            <TextInput
+              placeholder={'Enter title'}
+              onChangeText={text => setTitle(text)}
+              value={title}
+              style={{
+                height: SIZES.windowHeight / 12,
+                width: SIZES.windowWidth / 1.1,
+                backgroundColor: COLORS.lightGray,
+                paddingLeft: 10,
+                borderRadius: 10,
+                shadowColor: COLORS.primary,
+                elevation: 5,
+                borderWidth: 1,
+                borderColor: COLORS.lightGray,
+                justifyContent: 'center',
+                color: 'black',
+              }}
+            />
+          </View>
+
+          <View style={{marginTop: 15}}>
+            <TextInput
+              placeholder={'Enter year'}
+              onChangeText={text => setYear(text)}
+              value={year}
+              style={{
+                height: SIZES.windowHeight / 12,
+                width: SIZES.windowWidth / 1.1,
+                backgroundColor: COLORS.lightGray,
+                paddingLeft: 10,
+                borderRadius: 10,
+                shadowColor: COLORS.primary,
+                elevation: 5,
+                borderWidth: 1,
+                borderColor: COLORS.lightGray,
+                justifyContent: 'center',
+                color: 'black',
+              }}
+            />
+          </View>
+          <View style={{marginTop: 15}}>
+            <TextInput
+              placeholder={'Enter make'}
+              onChangeText={text => setMake(text)}
+              value={make}
+              style={{
+                height: SIZES.windowHeight / 12,
+                width: SIZES.windowWidth / 1.1,
+                backgroundColor: COLORS.lightGray,
+                paddingLeft: 10,
+                borderRadius: 10,
+                shadowColor: COLORS.primary,
+                elevation: 5,
+                borderWidth: 1,
+                borderColor: COLORS.lightGray,
+                justifyContent: 'center',
+                color: 'black',
+              }}
+            />
+          </View>
+          <View style={{marginTop: 15}}>
+            <TextInput
+              placeholder={'Enter model'}
+              onChangeText={text => setModel(text)}
+              value={model}
+              style={{
+                height: SIZES.windowHeight / 12,
+                width: SIZES.windowWidth / 1.1,
+                backgroundColor: COLORS.lightGray,
+                paddingLeft: 10,
+                borderRadius: 10,
+                shadowColor: COLORS.primary,
+                elevation: 5,
+                borderWidth: 1,
+                borderColor: COLORS.lightGray,
+                justifyContent: 'center',
+                color: 'black',
+              }}
+            />
+          </View>
+        </View>
+
+        <View
+          style={{paddingHorizontal: 20, paddingTop: 20, alignItems: 'center'}}>
+          <TouchableOpacity
+            style={{
+              height: SIZES.windowHeight / 18,
+              width: SIZES.windowWidth / 1.3,
+              borderRadius: 10,
+            }}
+            onPress={update}>
+            <LinearGradient
+              start={{x: 0, y: 0}}
+              end={{x: 1, y: 0}}
+              colors={['#1A72DE', 'rgba(35, 111, 204, 0.19)']}
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 10,
+              }}>
+              <Text style={{color: COLORS.white, fontSize: 16}}>Save</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
 }
