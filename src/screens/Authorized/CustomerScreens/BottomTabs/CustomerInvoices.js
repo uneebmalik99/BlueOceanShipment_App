1242c5d400db37950bfa585bf2e18cf1d9cb5413
Aamir <aamir104512@gmail.com>
2023-03-28 11:09:54 +0500

    invoices api

--- src/screens/Authorized/CustomerScreens/BottomTabs/CustomerInvoices.js
+++ src/screens/Authorized/CustomerScreens/BottomTabs/CustomerInvoices.js
@@ -1,14 +1,119 @@
-import {View, Text, TouchableOpacity, TextInput} from 'react-native';
-import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import AppBackground from '../../../../components/AppBackground';
 import {SIZES, COLORS, SVGBackground} from '../../../../constants/theme';
 import {SvgXml} from 'react-native-svg';
 import MaterialCommunity from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import LinearGradient from 'react-native-linear-gradient';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function CustomerInvoices({navigation}) {
+  const [allInvoices, setAllInvoices] = useState(null);
   const [invoiceTab, setInvoiceTab] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setAllInvoices(null);
+      setLoading(true);
+      try {
+        const token = await AsyncStorage.getItem('token');
+        if (token !== null) {
+          console.log('Token retrieved from AsyncStorage:', token);
+          try {
+            const response = await fetch(
+              'https://app.ecsapshipping.com/api/auth/invoice/allinvoices',
+              {
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                  Authorization: 'Bearer ' + token,
+                },
+              },
+            );
+
+            console.log('Fetching all invoices...');
+            const data = await response.json();
+
+            if (data.status == 'Success') {
+              setAllInvoices(data);
+              setLoading(false);
+              console.log('All Invoices fetched successfully');
+              console.log(data.status);
+            } else {
+              console.log('Error fetching all invoices');
+              setLoading(false);
+            }
+          } catch (error) {
+            console.error(error);
+            setLoading(false);
+          }
+        }
+      } catch (error) {
+        console.warn('Error while retrieving token from AsyncStorage:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const renderAllInvoices = ({item}) => {
+    return (
+      <View style={{flex: 1, marginTop: 10}}>
+        <TouchableOpacity
+          style={{
+            height: SIZES.windowHeight / 8,
+            width: SIZES.windowWidth,
+            paddingHorizontal: 20,
+          }}
+          // onPress={() => navigation.navigate('VehicleDetails', {Data: item})}
+        >
+          <LinearGradient
+            start={{x: 0, y: 0}}
+            end={{x: 1, y: 0}}
+            colors={['#1A72DE', 'rgba(35, 111, 204, 0.19)']}
+            style={{
+              flex: 1,
+              borderRadius: 15,
+              alignItems: 'center',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingHorizontal: 20,
+            }}>
+            <View>
+              <Text style={{color: COLORS.white}}>AR No: {item.ar_number}</Text>
+              <Text style={{color: COLORS.white, marginTop: 5}}>
+                Date: {item.invoice_date}
+              </Text>
+            </View>
+
+            <View>
+              <Text style={{color: COLORS.black}}>{item.invoice_amount}$</Text>
+              <View
+                style={{
+                  backgroundColor: 'green',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: 5,
+                  marginTop: 5,
+                }}>
+                <Text style={{color: COLORS.white}}>Paid</Text>
+              </View>
+            </View>
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   function hadleAllInvoices() {
     setInvoiceTab(1);
@@ -148,6 +253,7 @@ export default function CustomerInvoices({navigation}) {
           elevation: 3,
           alignItems: 'center',
         }}>
+        {/* tab (all, paid, unpaid, partially paid) buttons */}
         <View
           style={{
             height: SIZES.windowHeight / 14,
@@ -169,12 +275,16 @@ export default function CustomerInvoices({navigation}) {
               alignItems: 'center',
               justifyContent: 'center',
               flex: 1,
-              borderWidth: invoiceTab == 1 ? 0 : 1,
+              borderWidth: invoiceTab == 1 ? 0 : 0.5,
               borderColor: 'grey',
               borderRightWidth: 0,
             }}
             onPress={hadleAllInvoices}>
-            <Text style={{color: invoiceTab == 1 ? COLORS.white : 'grey'}}>
+            <Text
+              style={{
+                color: invoiceTab == 1 ? COLORS.white : 'grey',
+                fontSize: 12,
+              }}>
               All
             </Text>
           </TouchableOpacity>
@@ -186,12 +296,16 @@ export default function CustomerInvoices({navigation}) {
               alignItems: 'center',
               justifyContent: 'center',
               flex: 1,
-              borderWidth: invoiceTab == 2 ? 0 : 1,
+              borderWidth: invoiceTab == 2 ? 0 : 0.5,
               borderColor: 'grey',
               borderRightWidth: 0,
             }}
             onPress={hadlePaidInvoices}>
-            <Text style={{color: invoiceTab == 2 ? COLORS.white : 'grey'}}>
+            <Text
+              style={{
+                color: invoiceTab == 2 ? COLORS.white : 'grey',
+                fontSize: 12,
+              }}>
               Paid
             </Text>
           </TouchableOpacity>
@@ -203,12 +317,16 @@ export default function CustomerInvoices({navigation}) {
               alignItems: 'center',
               justifyContent: 'center',
               flex: 1,
-              borderWidth: invoiceTab == 3 ? 0 : 1,
+              borderWidth: invoiceTab == 3 ? 0 : 0.5,
               borderColor: 'grey',
               borderRightWidth: 0,
             }}
             onPress={hadleUnpaidInvoices}>
-            <Text style={{color: invoiceTab == 3 ? COLORS.white : 'grey'}}>
+            <Text
+              style={{
+                color: invoiceTab == 3 ? COLORS.white : 'grey',
+                fontSize: 12,
+              }}>
               Unpaid
             </Text>
           </TouchableOpacity>
@@ -221,106 +339,39 @@ export default function CustomerInvoices({navigation}) {
               borderBottomRightRadius: 50,
               alignItems: 'center',
               justifyContent: 'center',
-              borderWidth: invoiceTab == 4 ? 0 : 1,
+              borderWidth: invoiceTab == 4 ? 0 : 0.5,
               borderColor: 'grey',
               flex: 1,
             }}
             onPress={hadlePartialInvoices}>
-            <Text style={{color: invoiceTab == 4 ? COLORS.white : 'grey'}}>
+            <Text
+              style={{
+                color: invoiceTab == 4 ? COLORS.white : 'grey',
+                fontSize: 12,
+              }}>
               Partially Paid
             </Text>
           </TouchableOpacity>
         </View>
 
-        {invoiceTab == 1 && (
-          <View style={{marginTop: 30}}>
-            <TouchableOpacity
-              style={{
-                height: SIZES.windowHeight / 8,
-                width: SIZES.windowWidth,
-                paddingHorizontal: 20,
+        {invoiceTab == 1 && allInvoices != null && (
+          <View>
+            <FlatList
+              data={allInvoices.data}
+              contentContainerStyle={{
+                paddingBottom: '50%',
+                paddingTop: 10,
               }}
-              // onPress={() => navigation.navigate('VehicleDetails', {Data: item})}
-            >
-              <LinearGradient
-                start={{x: 0, y: 0}}
-                end={{x: 1, y: 0}}
-                colors={['#1A72DE', 'rgba(35, 111, 204, 0.19)']}
-                style={{
-                  flex: 1,
-                  borderRadius: 15,
-                  alignItems: 'center',
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  paddingHorizontal: 20,
-                }}>
-                <View>
-                  <Text style={{color: COLORS.white}}>Muhammad Aamir</Text>
-                  <Text style={{color: COLORS.white, marginTop: 5}}>
-                    07/03/2022
-                  </Text>
-                </View>
+              keyExtractor={item => item.vin}
+              renderItem={renderAllInvoices}
+            />
+          </View>
+        )}
 
-                <View>
-                  <Text style={{color: COLORS.black}}>2000$</Text>
-                  <View
-                    style={{
-                      backgroundColor: 'green',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      borderRadius: 5,
-                      marginTop: 5,
-                    }}>
-                    <Text style={{color: COLORS.white}}>Paid</Text>
-                  </View>
-                </View>
-              </LinearGradient>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              style={{
-                height: SIZES.windowHeight / 8,
-                width: SIZES.windowWidth,
-                marginTop: 10,
-                paddingHorizontal: 20,
-              }}
-              // onPress={() => navigation.navigate('VehicleDetails', {Data: item})}
-            >
-              <LinearGradient
-                start={{x: 0, y: 0}}
-                end={{x: 1, y: 0}}
-                colors={['#1A72DE', 'rgba(35, 111, 204, 0.19)']}
-                style={{
-                  flex: 1,
-                  borderRadius: 15,
-                  alignItems: 'center',
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  paddingHorizontal: 20,
-                }}>
-                <View>
-                  <Text style={{color: COLORS.white}}>Hassan Ali</Text>
-                  <Text style={{color: COLORS.white, marginTop: 5}}>
-                    07/03/2022
-                  </Text>
-                </View>
-
-                <View>
-                  <Text style={{color: COLORS.black}}>3629$</Text>
-                  <View
-                    style={{
-                      backgroundColor: 'red',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      borderRadius: 5,
-                      marginTop: 5,
-                      padding: 3,
-                    }}>
-                    <Text style={{color: COLORS.white}}>Unpaid</Text>
-                  </View>
-                </View>
-              </LinearGradient>
-            </TouchableOpacity>
+        {loading == true && (
+          <View
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <ActivityIndicator color={COLORS.primary} size={'large'} />
           </View>
         )}
       </View>
