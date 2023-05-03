d2f3d12e78cb7bb93c524e4fdb957f09e48b6a31
Aamir <aamir104512@gmail.com>
2023-05-03 18:00:51 +0500

    wathlist screen design

--- src/screens/Authorized/CustomerScreens/CustomerDrawer/Watchlist.js
+++ src/screens/Authorized/CustomerScreens/CustomerDrawer/Watchlist.js
@@ -1,10 +1,119 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {
+  SIZES,
+  COLORS,
+  SVGBackground,
+  SVGVehicle,
+} from '../../../../constants/theme';
+import {SvgXml} from 'react-native-svg';
+import MaterialCommunity from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AppBackground from '../../../../components/AppBackground';
+import IonIcons from 'react-native-vector-icons/Ionicons';
+import LinearGradient from 'react-native-linear-gradient';
 
-export default function Watchlist() {
+export default function Watchlist({navigation}) {
   return (
-    <View>
-      <Text>Watchlist</Text>
+    <View style={{flex: 1, backgroundColor: COLORS.white}}>
+      <View
+        style={{position: 'absolute', top: 0, left: 0, bottom: 0, right: 0}}>
+        <AppBackground />
+      </View>
+
+      {/* header with back button and screen name */}
+      <View
+        style={{
+          width: SIZES.windowWidth,
+          height: SIZES.windowHeight / 14,
+          backgroundColor: 'rgba(29, 119, 231, 0.81)',
+          justifyContent: 'space-between',
+          paddingHorizontal: 20,
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <IonIcons
+            name="arrow-back-circle-sharp"
+            size={25}
+            color={COLORS.white}
+          />
+        </TouchableOpacity>
+
+        <View>
+          <Text style={{color: COLORS.white, fontSize: 18}}>Watchlist</Text>
+        </View>
+
+        <View />
+      </View>
+
+      <View
+        style={{
+          backgroundColor: COLORS.white,
+          flex: 1,
+          width: '100%',
+          marginTop: 30,
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          shadowColor: COLORS.black,
+          elevation: 3,
+          // alignItems: 'center',
+        }}>
+        <View style={{position: 'absolute'}}>
+          <SvgXml
+            xml={SVGVehicle}
+            width={SIZES.windowWidth}
+            height={SIZES.windowHeight / 1.5}
+          />
+        </View>
+
+        {/* <View style={{alignItems: 'center', marginTop: 20}}>
+          <Text style={{fontSize: 18, color: COLORS.black, fontWeight: 'bold'}}>
+            Watchlist
+          </Text>
+        </View> */}
+
+        <View style={{flex: 1}}>
+          <TouchableOpacity
+            style={{
+              height: SIZES.windowHeight / 8,
+              width: SIZES.windowWidth,
+              marginTop: 10,
+              paddingHorizontal: 20,
+            }}
+            // onPress={() =>
+            //   navigation.navigate('VehicleDetails', {ID: item.id})}
+          >
+            <LinearGradient
+              start={{x: 0, y: 0}}
+              end={{x: 1, y: 0}}
+              colors={['#1A72DE', 'rgba(35, 111, 204, 0.19)']}
+              style={{
+                flex: 1,
+                borderRadius: 15,
+                justifyContent: 'center',
+                paddingHorizontal: 20,
+              }}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                }}>
+                <View>
+                  <Text style={{color: COLORS.white}}>Muhammad Aamir</Text>
+                  <Text style={{color: COLORS.black}}>0000000</Text>
+                  <Text style={{color: COLORS.black}}>21/09/2023</Text>
+                </View>
+
+                <View>
+                  <Text style={{color: COLORS.black}}>2300$</Text>
+                </View>
+              </View>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
